8a07cacaefe7ed4c2d057bc36da1dee249393a0b
Dell <Dell@MacBook-Air.local>
2025-05-21 21:39:40 +0530

    new

--- quetion1.java
+++ quetion1.java
@@ -2,5 +2,7 @@ public class quetion1 {
 
     public static void main(String[] args) {
         System.out.println("sneha pagal");
+        System.out.println("sneha pagal");
+        System.out.println("sneha pagal");
     }
 }
