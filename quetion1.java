1250867fc87149dc582a3ca6ee08567b1a084a87
Dell <Dell@MacBook-Air.local>
2025-05-21 21:48:54 +0530

    heey

--- quetion1.java
+++ quetion1.java
@@ -1,8 +1,6 @@
 public class quetion1 {
 
     public static void main(String[] args) {
-        System.out.println("sneha pagal");
-        System.out.println("sneha pagal");
-        System.out.println("sneha pagal");
+       
     }
 }
